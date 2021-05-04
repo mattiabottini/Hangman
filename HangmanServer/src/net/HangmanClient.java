214032a7605bead05a5445c5562fc6e55845e5ac
mattiabottini <mattia.bottini01@universitadipavia.it>
2021-05-04 15:53:46 +0200

    adding client class

---
+++ HangmanServer/src/net/HangmanClient.java
@@ -0,0 +1,13 @@
+package net;
+
+import java.io.*;
+
+public class HangmanClient {
+
+    int porta = 6789;
+    DataInputStream in;
+    DataOutputStream out = null;
+    BufferedReader tastiera = null;
+    String riga;
+
+}
